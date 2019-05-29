3316acd15e68cc1d107c6337a6fec65a15a213e8
Classic-username <josh.drapergame@gmail.com>
2019-05-29 17:38:58 -0600

    project complete

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -35,7 +35,7 @@ GameObject.prototype.destroy = function(){
 */
 
 let CharacterStats = function(stattributes){
-  GameObject.call(this, ...stattributes);
+  GameObject.call(this, stattributes);
   this.healthPoints = stattributes.healthPoints;
 }
 
@@ -56,7 +56,7 @@ CharacterStats.prototype.takeDamage = function(){
 */
 
 let Humanoid = function(oidtributes){
-  CharacterStats.call(this, ...oidtributes)
+  CharacterStats.call(this, oidtributes)
   this.team = oidtributes.team;
   this.weapons = oidtributes.weapons;
   this.language = oidtributes.language;
@@ -76,7 +76,7 @@ Humanoid.prototype.greet = function(){
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -137,7 +137,7 @@ Humanoid.prototype.greet = function(){
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
